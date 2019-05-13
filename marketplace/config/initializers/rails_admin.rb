7d9e144260ceeae6b212b2637afbf87114197663
leandro lasnor <llasnor877@beijaflore.com>
2019-05-12 23:48:42 -0300

    Import Products

--- marketplace/config/initializers/rails_admin.rb
+++ marketplace/config/initializers/rails_admin.rb
@@ -1,28 +1,10 @@
 require Rails.root.join('lib', 'rails_admin', 'introduce_products.rb')
 require 'open-uri' 
 RailsAdmin.config do |config|
-  ### Popular gems integration
 
-  ## == Devise ==
-  # config.authenticate_with do
-  #   warden.authenticate! scope: :user
-  # end
-  # config.current_user_method(&:current_user)
-
-  ## == Cancan ==
   config.authorize_with :cancan
   config.current_user_method(&:current_user)
-  ## == Pundit ==
-  # config.authorize_with :pundit
 
-  ## == PaperTrail ==
-  # config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
-
-  ### More at https://github.com/sferik/rails_admin/wiki/Base-configuration
-
-  ## == Gravatar integration ==
-  ## To disable Gravatar integration in Navigation Bar set to false
-  # config.show_gravatar = true
   config.model Store do
     create do
       fields :name, :website, :email, :logo
